e7553eba6d38fbffbe83c45d242de5e26a4598ba
vojhab <voj.hab@proton.me>
2023-04-09 19:10:22 +0200

    Index page working

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
