911ce8babf0caee9c2b23fc6c32cb0985aa23d16
vojhab <voj.hab@proton.me>
2023-04-10 07:45:21 +0200

    Separated routes

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+const port = 3000;
+const viewsPath = path.join(__dirname, "views");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.set("views", `${__dirname}/views`);
+app.set("views", viewsPath);
 
-app.get("/", (req, res) => {
-  res.render("index", { title: "Ding Search" });
-});
+const indexRouter = require("./routes/index");
+app.use("/", indexRouter);
 
-const port = 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
