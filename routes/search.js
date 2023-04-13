b038e86d0b9ee6893c0767edbf6f4e5714486290
vojhab <voj.hab@proton.me>
2023-04-13 17:30:10 +0200

    Working with Postgresql

--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,44 @@
 const express = require("express");
 const router = express.Router();
+const { Pool } = require("pg");
 
-router.get("/", (req, res) => {
-  const question = req.query.question;
-  res.render("search", { title: `${question} - Ding Search` });
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+router.get("/", async (req, res) => {
+  const query = req.query.q;
+
+  if (!query) {
+    res.redirect("/");
+  }
+
+  const client = await pool.connect();
+
+  const databaseQuery = {
+    text: `select id, title, url, description,
+  ts_rank(search, websearch_to_tsquery('english', $1)) +
+  ts_rank(search, websearch_to_tsquery('simple', $1))
+  as rank
+  from crawled
+  where search @@ websearch_to_tsquery('english', $1)
+  or search @@ websearch_to_tsquery('simple', $1)
+  order by rank desc;`,
+    values: [query],
+  };
+
+  const results = await client.query(databaseQuery);
+
+  client.release();
+
+  res.render("search", {
+    title: `${query} - Ding Search`,
+    results: results.rows,
+  });
 });
 
 module.exports = router;
